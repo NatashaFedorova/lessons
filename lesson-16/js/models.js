41628e36f207b2ecd709e7b222dfb35f95f1277f
NataliaFedorova <98808643+NatashaFedorova@users.noreply.github.com>
2024-02-27 18:41:27 +0200

    Add files via upload

---
+++ lesson-16/js/models.js
@@ -0,0 +1,4 @@
+import refs from './refs.js';
+import onSubmit from './onSubmit.js';
+
+refs.myInfoForm.addEventListener('submit', onSubmit);
---
+++ lesson-16/js/refs.js
@@ -0,0 +1,6 @@
+const refs = {
+   practiceForm: document.querySelector('[data-practice-form]'),
+   myInfoForm: document.querySelector('[data-my-info-form]'),
+};
+
+export default refs;
